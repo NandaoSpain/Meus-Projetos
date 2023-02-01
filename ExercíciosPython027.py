e23f90c5232b7446509df9fcb7f7bd96264ec680
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2023-02-01 17:21:20 -0300

    Create ExercíciosPython027.py

---
+++ ExercíciosPython027.py
@@ -0,0 +1,10 @@
+def área(larg, comp):
+    a = (larg * comp)
+    print(f'A área de um terreno de {larg:.2f}m x {comp:.2f}m é de {a:.2f}m')
+
+
+print('Controle de Terreno')
+print('-' * 20)
+l = float(input('Largura (m) :'))
+c = float(input('Comprimento (m): '))
+área(l, c)
