d4a41dc383e5e99d48f90e7c4f81dd7e613856da
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2023-02-01 17:31:49 -0300

    Create ExercíciosPython028.py

---
+++ ExercíciosPython028.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tam = len(msg)
+    print('~' * tam)
+    print(msg)
+    print('~' * tam)
+
+
+escreva('Hello, World!')
+escreva('Curso em vídeo')
+escreva(f'{"CEV":^18}')
+escreva('Bora lá aprender Python')
