fbd148c92518857c2af092030d1a77d34e720538
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2023-02-03 11:50:07 -0300

    Create ExercíciosPython033.py

---
+++ ExercíciosPython033.py
@@ -0,0 +1,21 @@
+def fatorial(n, show=False):
+    """
+    --> Calcula o fatorial de um número
+    :param n: O número a ser calculado
+    :param show: (opcional) mostrar ou não a conta.
+    :return: O valor do fatorial de um número n.
+    """
+    f = 1
+    print('-' * 23)
+    for c in range(n, 0, -1):
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        f *= c
+    return f
+
+
+print(fatorial(5, show=True))
