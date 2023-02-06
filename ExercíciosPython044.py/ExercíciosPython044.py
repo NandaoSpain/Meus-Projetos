760e06bcdcb1e4528a60ba54a3f006b698b51391
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2023-02-06 11:29:31 -0300

    047

--- ExercíciosPython044.py/ExercíciosPython044.py
+++ ExercíciosPython044.py/ExercíciosPython044.py
@@ -1,7 +1,4 @@
 from ExerciciosPython.Ex046 import moeda
 
 num = (int(input('Digite o Preço: R$')))
-print(f'O dobro de {moeda.din(num)} é {moeda.dobro(num, True)}')
-print(f'A metade de {moeda.din(num)} é {moeda.metade(num, True)}')
-print(f'Aumentando 10%, temos {moeda.aumentar(num,  10, True)}')
-print(f'Diminuindo 18%, temos {moeda.aumentar(num, 18, True)}')
+moeda.resumo(num, 20, 12)
--- ExercíciosPython044.py/moeda.py
+++ ExercíciosPython044.py/moeda.py
@@ -20,3 +20,20 @@ def metade(preço=0, formato=False):
 
 def din(preço=0, moeda='R$'):
     return f'{moeda}{preço:.2f}'.replace('.', ',')
+
+
+def resumo(preço=0, aum=0, red=0):
+    dic = {'Preço Analisado:', preço,
+           'Dobro do Preço:', dobro(preço),
+           'Metade do Preço:', metade(preço),
+           f'{aum} de Aumento:', aumentar(preço),
+           f'{red} de Redução:', diminuir(preço)}
+    print('-' * 30)
+    print(f'{"Resumo do Valor":^30}')
+    print('-' * 30)
+    print(f'Preço Analisado:   \t{din(preço):>}')
+    print(f'Dobro do Preço:    \t{dobro(preço, True):>}')
+    print(f'Metade do Preço:   \t{metade(preço, True):>}')
+    print(f'Aumento de {aum}%: \t{aumentar(preço, aum, True):>}')
+    print(f'Redução de {red}%: \t{diminuir(preço, red, True):>}')
+    print('-' * 30)
