7624b5c93dd19db56e23bed67204f20b4bb38c25
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2023-02-06 10:39:03 -0300

    046

--- ExercíciosPython044.py/ExercíciosPython044.py
+++ ExercíciosPython044.py/ExercíciosPython044.py
@@ -1,6 +1,7 @@
-from ExerciciosPython.Ex045 import moeda
+from ExerciciosPython.Ex046 import moeda
 
 num = (int(input('Digite o Preço: R$')))
-print(f'O dobro de {moeda.din(num)} é {moeda.din(moeda.dobro(num))}')
-print(f'A metade de {moeda.din(num)} é {moeda.din(moeda.metade(num))}')
-print(f'Aumentando 10%, temos {moeda.din(moeda.aumentar(num, 10))}')
+print(f'O dobro de {moeda.din(num)} é {moeda.dobro(num, True)}')
+print(f'A metade de {moeda.din(num)} é {moeda.metade(num, True)}')
+print(f'Aumentando 10%, temos {moeda.aumentar(num,  10, True)}')
+print(f'Diminuindo 18%, temos {moeda.aumentar(num, 18, True)}')
--- ExercíciosPython044.py/moeda.py
+++ ExercíciosPython044.py/moeda.py
@@ -1,21 +1,21 @@
-def aumentar(preço=0, taxa=0):
+def aumentar(preço=0, taxa=0, formato=False):
     res = preço + (preço * taxa / 100)
-    return res
+    return res if not formato else din(res)
 
 
-def diminuir(preço=0, taxa=0):
+def diminuir(preço=0, taxa=0, formato=False):
     res = preço - (preço * taxa / 100)
-    return res
+    return res if not formato else din(res)
 
 
-def dobro(preço=0):
+def dobro(preço=0, formato=False):
     res = preço * 2
-    return res
+    return res if not formato else din(res)
 
 
-def metade(preço=0):
+def metade(preço=0, formato=False):
     res = preço / 2
-    return res
+    return res if not formato else din(res)
 
 
 def din(preço=0, moeda='R$'):
