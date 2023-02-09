a59c44d261097f7df36a0fac212a0a66d934e9c1
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2023-02-09 16:30:39 -0300

    Create Exercício001.py

---
+++ Curso_Udemy/Exercício001.py
@@ -0,0 +1,13 @@
+nome = str(input('Digite seu nome: '))
+idade = int(input('Digite sua idade: '))
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+    if ' ' in nome:
+        print('Seu nome contém espaços!')
+    else:
+        print('Seu nome NÃO contém espaços!')
+    print(f'Seu nome tem {len(nome)} letras.')
+    print(f'A última letra do seu nome é {nome[-1]}')
+else:
+    print('Desculpe, você deixou campos vazios!')
