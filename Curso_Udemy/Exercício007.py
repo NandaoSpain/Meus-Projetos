b7a957f59e2d1179cc11361322b483640ffd8420
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2023-02-14 13:02:45 -0300

    Update Exercício007.py

--- Curso_Udemy/Exercício007.py
+++ Curso_Udemy/Exercício007.py
@@ -1,7 +1,7 @@
 print('Validador de CPF'.center(40))
-cpf = '060.851.029-76'
-cpf_puro = []
-multiplicador = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+cpf = input('Digite um CPF para comprovação: ')
+cpf_puro = []   
+multiplicador1 = 10
 for c in cpf:
     if c != '.' and c != '-':
         inteiro = int(c)
@@ -9,13 +9,23 @@ for c in cpf:
 cpf_manipular = cpf_puro[:]
 cpf_puro.pop(-1)
 cpf_puro.pop(-1)
-pos = 0
-resultado1 = 0
+resultadosoma1 = 0
 for c in cpf_puro:
-    resultado1 += c * multiplicador[pos]
-    pos += 1
-resultado2 = resultado1 * 10
-resultado3 = resultado2 % 11
-cpf_puro.append(resultado3)
-print(cpf_puro)
+    resultadosoma1 += c * multiplicador1
+    multiplicador1 -= 1 
+resultado = resultadosoma1 * 10 % 11
+primeiro_digito = resultado if resultado <= 9 else 0
+cpf_puro.append(primeiro_digito)
 
+resultadosoma2 = 0
+multiplicador2 = 11
+for c in cpf_puro:
+    resultadosoma2 += c * multiplicador2
+    multiplicador2 -= 1
+resultado2 = resultadosoma2 * 10 % 11
+segundo_digito = resultado2 if resultado2 <=9 else 0
+cpf_puro.append(segundo_digito)
+if cpf_puro == cpf_manipular:
+    print('CPF Válido!')
+else:
+    print('CPF INVÁLIDO!')
