369d4515752a23421fd09c2fae4e3eed87b97ff9
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2023-02-17 15:03:49 -0300

    Create PerguntasErespostas.py

---
+++ Curso_Udemy/PerguntasErespostas.py
@@ -0,0 +1,28 @@
+perguntas = [
+    {
+        'Pergunta': 'Quanto é 2+2?',
+        'Opções': ['1', '3', '4', '5'],
+        'Resposta': '4',
+    },
+    {
+        'Pergunta': 'Quanto é 5*5?',
+        'Opções': ['25', '55', '10', '51'],
+        'Resposta': '25',
+    },
+    {
+        'Pergunta': 'Quanto é 10/2?',
+        'Opções': ['4', '5', '2', '1'],
+        'Resposta': '5',
+    },
+]
+
+
+print('Jogo do Milhão'.center(20))
+for pergunta in perguntas:
+    print()
+    print('Pergunta: ', pergunta['Pergunta'])
+    print()
+    print('Opção: ')
+    print()
+    for opcao in pergunta['Opções']:
+        print(opcao)
