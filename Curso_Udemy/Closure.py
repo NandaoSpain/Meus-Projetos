782fbeaa0c0fb57cc123b06a4ad22200dc9d1dff
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2023-02-23 15:18:48 -0300

    Update Closure.py

--- Curso_Udemy/Closure.py
+++ Curso_Udemy/Closure.py
@@ -10,3 +10,15 @@ quituplo = multiplicar(5)
 
 print(dobro(5))
 print(quituplo(1985))
+
+
+def nome(nombre):
+    def upper(nombre):
+        return nombre.upper()
+    return upper
+
+@nome
+def imprimir(nome):
+    return f'Seu nome é {nome}'
+
+print(imprimir('Fernando'))
