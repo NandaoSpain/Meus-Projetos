02901ef48a0774e997df9c27245f92aa561f3f88
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2023-02-23 16:13:09 -0300

    Update Exercicio016.py

--- Curso_Udemy/Exercicio016.py
+++ Curso_Udemy/Exercicio016.py
@@ -1,7 +1,9 @@
+from itertools import zip_longest
+
 lista_a = [10, 2, 3, 40, 5, 6, 7]
 lista_b = [1, 2, 3, 4]
 
-lista_soma = [x + y for x, y in zip(lista_a, lista_b)]
+lista_soma = [x + y for x, y in zip_longest(lista_a, lista_b, fillvalue=0)]
 print(lista_soma)
 
 
