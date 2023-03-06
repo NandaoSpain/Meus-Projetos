36bf00df33f1810547c353a6a5b32904e90fe8d9
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2023-03-06 17:31:27 -0300

    Update Exercício018.py

--- Curso_Udemy/Exercício018.py
+++ Curso_Udemy/Exercício018.py
@@ -1,3 +1,4 @@
+import json
 caminho_arquivo = 'C:\\Users\\Nandão\\Desktop\\Arquivo\\Exercício018.txt'
 
 arquivo = open(caminho_arquivo, 'w+')
@@ -7,3 +8,20 @@ arquivo.close()
 
 with open(caminho_arquivo, 'w+') as arquivo:
     ...
+
+
+pessoa = {
+    'nome': 'Luiz Otávio 2',
+    'sobrenome': 'Miranda',
+    'enderecos': [
+        {'rua': 'R1', 'numero': 32},
+        {'rua': 'R2', 'numero': 55},
+    ],
+    'altura': 1.8,
+    'numeros_preferidos': (2, 4, 6, 8, 10),
+    'dev': True,
+    'nada': None,
+}
+
+with open('aula117.json', 'w', encoding='utf-8') as arquivo:
+    json.dump(pessoa, arquivo, ensure_ascii=False, indent=2)
